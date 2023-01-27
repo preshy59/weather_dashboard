1d6aa04ae50ad8840afe50a0d4397c05c2b61007
Ebele Okemba <okemba50@gmail.com>
2023-01-27 20:30:09 +0000

    create an event listerner attached to the search button that storages the input in the local storage and attached other functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,29 @@ let searchBtn = document.querySelector("#search-button");
 let searchInput = document.querySelector("search-input");
 let todayWeather = document.querySelector("#today");
     
+let apiKey = "f4c9518fc9bfcf20e6accc10273d3d66";
+let variousCities = [];
 
+searchBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    let cities = searchInput.value;
+
+    weatherSearch(cities);
+
+    if (!variousCities.includes(cities)) {
+        variousCities.push(city);
+        cityStorage();
+    }
+
+    function cityStorage() {
+        localStorage.setItem("variousCities", JSON.stringify(variousCities));
+        console.log(localStorage);
+        
+    }
+    renderCities(variousCities);
+
+    
+});
 
    
