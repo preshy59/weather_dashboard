618ddabc8d0608901e5155846d095c8906209561
Ebele Okemba <okemba50@gmail.com>
2023-01-27 20:15:49 +0000

    created a DOM element using the query selector

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//DOM ELEMENT
+let searchBtn = document.querySelector("#search-button");
+let searchInput = document.querySelector("search-input");
+let todayWeather = document.querySelector("#today");
+    
+
+
+   
